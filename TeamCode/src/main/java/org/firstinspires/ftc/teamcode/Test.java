b19dc7d9fea5280b2b59e134ed5bf84b43081836
WalkerWilcoxon <wawilcoxon@student.linnmar.org>
2017-09-09 18:56:35 -0500

    Reformatting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -3,15 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-/**
- * Created by LMRobotics on 9/9/2017.
- */
-
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Test: Teleop Tank", group="Test")
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Test: Teleop Tank", group = "Test")
 public class Test extends OpMode {
     private DcMotor leftmotor;
     private DcMotor rightmotor;
-
 
 
     @Override
