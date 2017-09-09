b7c871092223181a911b55d7bc1deacb20be28c0
Lost in Time <lostintime4324@gmail.com>
2017-09-09 17:13:57 -0500

    Revert "Revert "Base code""
    This reverts commit 8ae5e27cdf3dd1bc47844d7a632748ec08354eb2.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Pompeii: Teleop Tank", group="Pompeii")
+public class TeleOp extends OpMode {
+    // TeleOp
+    private MotorController leftMotors;
+    private MotorController rightMotors;
+
+    @Override
+    public void init() {
+        leftMotors = new MotorController(hardwareMap);
+        leftMotors.add(MotorNames.BACK_LEFT_DRIVE.get());
+        leftMotors.add(MotorNames.FRONT_LEFT_DRIVE.get());
+        rightMotors = new MotorController(hardwareMap);
+        rightMotors.add(MotorNames.BACK_RIGHT_DRIVE.get());
+        rightMotors.add(MotorNames.FRONT_RIGHT_DRIVE.get());
+    }
+
+    @Override
+    public void loop() {
+        leftMotors.setPower(gamepad1.left_stick_y);
+        rightMotors.setPower(gamepad1.right_stick_y);
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -1,1 +1,32 @@
-new java file
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by LMRobotics on 9/9/2017.
+ */
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Test: Teleop Tank", group="Test")
+public class Test extends OpMode {
+    private DcMotor leftmotor;
+    private DcMotor rightmotor;
+
+
+
+    @Override
+    public void init() {
+        leftmotor = hardwareMap.dcMotor.get("Motor1");
+        rightmotor = hardwareMap.dcMotor.get("Motor2");
+    }
+
+    @Override
+    public void loop() {
+        leftmotor.setPower(gamepad1.left_stick_y);
+        rightmotor.setPower(gamepad1.right_stick_y);
+        if (gamepad1.x) {
+            leftmotor.setPower(0);
+        }
+
+    }
+}
